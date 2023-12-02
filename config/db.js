42f70c9c6f20ac98122300f2bf59fbff6f5a9fa8
kelvinTest05 <testkelvin05@gmail.com>
2023-12-02 23:17:04 +0530

    new commit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose =require('mongoose')
 const connectDb=async ()=>{
     try{
-        const connection=await mongoose.connect('mongodb://127.0.0.1:27017/bookmycourt',{
+        // mongodb+srv://kelvingeorge:<password>@cluster0.a9vgnxb.mongodb.net/?retryWrites=true&w=majority
+    //    mongodb://127.0.0.1:27017/bookmycourt
+        const connection=await mongoose.connect('mongodb+srv://kelvingeorge:hf7HqdYSXzOpicid@cluster0.a9vgnxb.mongodb.net/?retryWrites=true&w=majority',{
             useNewUrlParser:'true'
         })
         console.log("MongoDb data base connected");
