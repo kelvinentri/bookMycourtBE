b9cb0f39a8d38e22422746ccb35619c31642b5a2
kelvinTest05 <testkelvin05@gmail.com>
2023-12-02 21:54:04 +0530

    cors issue updation

--- app.js
+++ app.js
@@ -27,7 +27,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
-app.use(cors()) // Use this after the variable declaration
+// app.use(cors()) // Use this after the variable declaration
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
