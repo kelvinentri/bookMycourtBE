be5ab2935ba405f72aaa3412a6e46bf1322406c3
kelvinTest05 <testkelvin05@gmail.com>
2023-12-03 14:47:41 +0530

    new

--- app.js
+++ app.js
@@ -33,15 +33,15 @@ app.set('view engine', 'jade');
 //   next();
 // });
 
-// app.use(cors()) // Use this after the variable declaration
-const corsOptions = {
-  origin: ['https://bookmycourt-fe.onrender.com', 'http://localhost:3000'],
-  credentials: true,
-  optionSuccessStatus: 200,
-};
+app.use(cors({ origin: ['https://bookmycourt-fe.onrender.com', 'http://localhost:3000'],})) // Use this after the variable declaration
+// const corsOptions = {
+//   origin: ['https://bookmycourt-fe.onrender.com', 'http://localhost:3000'],
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// };
 
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
