803c070dffe8542d32959e0bc62d266f6e2f0d4e
kelvinTest05 <testkelvin05@gmail.com>
2023-12-03 22:01:03 +0530

    f

--- app.js
+++ app.js
@@ -26,22 +26,16 @@ connectDb()
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// app.use((_req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', '*');
 
-//   next();
-// });
-
-app.use(cors()) // Use this after the variable declaration
-// const corsOptions = {
-//   origin: ['https://bookmycourt-fe.onrender.com', 'http://localhost:3000'],
-//   credentials: true,
-//   optionSuccessStatus: 200,
-// };
+// app.use(cors()) // Use this after the variable declaration
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
