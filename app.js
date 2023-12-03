f931810b671f47690609b89cada71fc7c42507b6
kelvinTest05 <testkelvin05@gmail.com>
2023-12-03 21:15:07 +0530

    new message

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.set('view engine', 'jade');
 //   next();
 // });
 
-app.use(cors({ origin: ['https://runsports.onrender.com', 'http://localhost:3000'],})) // Use this after the variable declaration
+app.use(cors()) // Use this after the variable declaration
 // const corsOptions = {
 //   origin: ['https://bookmycourt-fe.onrender.com', 'http://localhost:3000'],
 //   credentials: true,
