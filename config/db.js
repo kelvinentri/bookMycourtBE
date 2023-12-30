45318bcadd7c8956ef48a46c0ed5d2f434a7e783
kelvinTest05 <testkelvin05@gmail.com>
2023-12-30 21:17:43 +0530

    new

--- config/db.js
+++ config/db.js
@@ -3,9 +3,7 @@ const connectDb=async ()=>{
     try{
         // mongodb+srv://kelvingeorge:<password>@cluster0.a9vgnxb.mongodb.net/?retryWrites=true&w=majority
     //    mongodb://127.0.0.1:27017/bookmycourt
-        const connection=await mongoose.connect('mongodb+srv://kelvingeorge:hf7HqdYSXzOpicid@cluster0.a9vgnxb.mongodb.net/?retryWrites=true&w=majority',{
-            useNewUrlParser:'true'
-        })
+        const connection=await mongoose.connect('mongodb+srv://kelvingeorge:BxJNJB65l6n1r9kz@cluster0.aou7wev.mongodb.net/bookmycourte19')
         console.log("MongoDb data base connected");
     }
     catch(err){
